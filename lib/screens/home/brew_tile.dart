06c3d0fb70b82f9db5f2f1289dac9d71539fa30b
Yacine Boudebouz <104728737+yacineboudebouz@users.noreply.github.com>
2024-01-26 21:50:05 +0100

    Update brew_tile.dart

--- lib/screens/home/brew_tile.dart
+++ lib/screens/home/brew_tile.dart
@@ -10,8 +10,10 @@ class BrewTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(top: 8.0),
+
       child: Card(
         margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0),
+
         child: ListTile(
           leading: CircleAvatar(
             radius: 25.0,
